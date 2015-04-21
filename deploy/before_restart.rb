7d980c1ec9c6171a1c37d9b40fd2ee8543e5918b
josephcarlson <josephd.carlson@gmail.com>
2015-04-20 21:45:23 -0400

    changed deploy hooks

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,1 +1,1 @@
-run "cd /srv/www/ikeminds_dev/current && /usr/local/bin/bundle exec rake assets:precompile"
+run "cd /srv/www/likeminds_dev/current && /usr/local/bin/bundle exec rake assets:precompile"
