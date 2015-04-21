fc27498892ecfcd52d5d08db560f4497e4df5146
josephcarlson <josephd.carlson@gmail.com>
2015-04-20 21:59:40 -0400

    and again

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,1 +1,19 @@
-run "cd /srv/www/likeminds/current && /usr/local/bin/bundle exec rake assets:precompile"
+Chef::Log.info("Running deploy/before_restart.rb...")
+
+rails_env = new_resource.environment["RAILS_ENV"]
+Chef::Log.info("Precompiling assets for #{rails_env}...")
+
+current_release = release_path
+
+node[:deploy].each do |application, deploy|
+  deploy[:environment_variables].each do |key, value|
+    Chef::Log.info("Setting ENV[#{key}] to #{value}")
+    ENV[key] = value
+  end
+end
+
+execute "rake assets:precompile" do
+  cwd release_path
+  command "bundle exec rake assets:precompile"
+  environment "RAILS_ENV" => rails_env
+end
