b14afe74718b54ac33c183de2329b8a0f8aabdb8
josephcarlson <josephd.carlson@gmail.com>
2015-04-24 21:03:26 -0400

    Adding rails env definition

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,3 +1,6 @@
+rails_env = new_resource.environment["RAILS_ENV"]
+Chef::Log.info("Precompiling assets for #{rails_env}...")
+
 execute "rake assets:precompile" do
   cwd release_path
   command "bundle exec rake assets:precompile"
