a00505b89b50bc22a65c5b735402db0bb7efd740
josephcarlson <josephd.carlson@gmail.com>
2015-05-04 22:41:27 -0400

    Fixed migration scripts for deployment

--- db/migrate/20150314005920_create_interests.rb
+++ db/migrate/20150314005920_create_interests.rb
@@ -1,12 +1,12 @@
 class CreateInterests < ActiveRecord::Migration
   def change
-    create_table :interests do |t|
-      t.references :users, index: true
-      t.references :idea, index: true
-
-      t.timestamps null: false
-    end
-    add_foreign_key :interests, :users
-    add_foreign_key :interests, :ideas
+    # create_table :interests do |t|
+    #   t.references :users, index: true
+    #   t.references :idea, index: true
+    #
+    #   t.timestamps null: false
+    # end
+    # add_foreign_key :interests, :users
+    # add_foreign_key :interests, :ideas
   end
 end
--- db/migrate/20150316234958_add_is_columns_to_interests.rb
+++ db/migrate/20150316234958_add_is_columns_to_interests.rb
@@ -1,6 +1,8 @@
 class AddIsColumnsToInterests < ActiveRecord::Migration
   def change
     self.up
+    add_column :interests, :idea_id, :integer
+    add_column :interests, :user_id, :integer
     add_column :interests, :IsInterested, :boolean
     add_column :interests, :Comment, :text
   end
