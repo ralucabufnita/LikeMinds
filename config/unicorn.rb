7f72e658ed1704c5334ea492d09909d363db746f
josephcarlson <josephd.carlson@gmail.com>
2015-05-12 20:31:57 -0400

    revise unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,35 +1,24 @@
+# Set the working application directory
+# working_directory "/path/to/your/app"
+working_directory "/var/www/likeminds"
+
+# Unicorn PID file location
+# pid "/path/to/pids/unicorn.pid"
+pid "/var/www/my_app/pids/unicorn.pid"
+
+# Path to logs
+# stderr_path "/path/to/log/unicorn.log"
+# stdout_path "/path/to/log/unicorn.log"
+stderr_path "/var/www/my_app/log/unicorn.log"
+stdout_path "/var/www/my_app/log/unicorn.log"
+
+# Unicorn socket
+listen "/tmp/unicorn.likeminds.sock"
+listen "/tmp/unicorn.likeminds.sock"
+
+# Number of processes
+# worker_processes 4
 worker_processes 2
-working_directory "/var/rails/likeminds/"
 
-# This loads the application in the master process before forking
-# worker processes
-# Read more about it here:
-# http://unicorn.bogomips.org/Unicorn/Configurator.html
-preload_app true
-
+# Time-out
 timeout 30
-
-# This is where we specify the socket.
-# We will point the upstream Nginx module to this socket later on
-listen "/srv/www/likeminds/shared/sockets/unicorn.sock", :backlog => 64
-
-pid "/var/rails/testapp/tmp/pids/unicorn.pid"
-
-# Set the path of the log files inside the log folder of the testapp
-stderr_path "/var/rails/likeminds/log/unicorn.stderr.log"
-stdout_path "/var/rails/likeminds/log/unicorn.stdout.log"
-
-before_fork do |server, worker|
-# This option works in together with preload_app true setting
-# What is does is prevent the master process from holding
-# the database connection
-  defined?(ActiveRecord::Base) and
-      ActiveRecord::Base.connection.disconnect!
-end
-
-after_fork do |server, worker|
-# Here we are establishing the connection after forking worker
-# processes
-  defined?(ActiveRecord::Base) and
-      ActiveRecord::Base.establish_connection
-end
