dd5396de6a5fb5055667dad1f6ed12a04d863852
josephcarlson <josephd.carlson@gmail.com>
2015-05-13 19:50:20 -0400

    Correcting unicorn routes

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,20 +1,21 @@
+
 # Set the working application directory
 # working_directory "/path/to/your/app"
-working_directory "/var/www/likeminds"
+working_directory "/srv/www/likeminds/current/"
 
 # Unicorn PID file location
 # pid "/path/to/pids/unicorn.pid"
-pid "/var/www/my_app/pids/unicorn.pid"
+pid "/srv/www/likeminds/shared/pids/unicorn.pid"
 
 # Path to logs
 # stderr_path "/path/to/log/unicorn.log"
 # stdout_path "/path/to/log/unicorn.log"
-stderr_path "/var/www/my_app/log/unicorn.log"
-stdout_path "/var/www/my_app/log/unicorn.log"
+stderr_path "/srv/www/likeminds/shared/log/unicorn.log"
+stdout_path "/srv/www/likeminds/shared/log/unicorn.log"
 
 # Unicorn socket
-listen "/tmp/unicorn.likeminds.sock"
-listen "/tmp/unicorn.likeminds.sock"
+listen "/srv/www/likeminds/shared/sockets/unicorn.likeminds.sock"
+listen "/srv/www/likeminds/shared/sockets/unicorn.likeminds.sock"
 
 # Number of processes
 # worker_processes 4
